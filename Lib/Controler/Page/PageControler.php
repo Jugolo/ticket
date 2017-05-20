e341447a9cdee33dd2ac73101c755a11c9c5401d
Ronnie <rix17172@gmail.com>
2017-05-20 21:23:59 +0200

    Create PageControler.php

---
+++ Lib/Controler/Page/PageControler.php
@@ -0,0 +1,57 @@
+<?php
+namespace Lib\Controler\Page;
+
+class PageControler{
+  public static function getPageInfo($callback){
+    if(is_callable($callback)){
+      $dir = "Lib/Ext/Page/";
+      $stream = opendir($dir);
+      while($i = readdir($stream)){
+        if($i == "." || $i == ".."){
+          continue;
+        }
+        
+        if(is_dir($dir.$i) && file_exists($dir.$i."/Info.php")){
+          $c = "Lib\\Ext\\Page\\".$i."\\Info";
+          if(!class_exists($c)){
+            include $dir.$i."/Info.php";
+            if(!class_exists($c)){
+              continue;
+            }
+          }
+          
+          $obj = new $c();
+          if(!($obj instanceof PageInfo)){
+            continue;
+          }
+          
+          $data = call_user_func($callback, $obj);
+          if($data){
+            return $data;
+          }
+        }
+      }
+      closedir($stream);
+    }else{
+      trigger_error(E_USER_ERROR, "PageControler::getPageInfo() need a callable as agument");
+    }
+  }
+  
+  public static function getPage(){
+    $page = empty($_GET["view"]) ? "front" : $_GET["view"];
+    return self::getPageInfo(function(PageInfo $info) use($page){
+      if($info->name() === $page){
+        if(!$info->pageVisible()){
+          notfound();
+          return;
+        }
+        $class = get_class($info);
+        $class = substr($class, 0, strrpos($class, "\\")+1)."PageView";
+        $view = new $class();
+        if($view instanceof PageView){
+          return $view;
+        }
+      }
+    });
+  }
+}
