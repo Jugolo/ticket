1541d740e42c23b9c7b4724a95d0d9705efc8b8e
Ronnie <rix17172@gmail.com>
2017-05-20 21:07:15 +0200

    Create Access.php

---
+++ Lib/Ext/Page/Group/Sub/Access.php
@@ -0,0 +1,88 @@
+<?php
+namespace Lib\Ext\Page\Group\Sub;
+
+use Lib\Database;
+
+class Access{
+  public static function body(){
+    if(empty($_GET["gid"])){
+      notfound();
+      return;
+    }
+    
+    $ugroup = getUsergroup(user["groupid"]);
+    $group = $ugroup["id"] == $_GET["gid"] ? $ugroup : getUsergroup($_GET["gid"]);
+    if($ugroup["handleGroup"] != 1){
+      notfound();
+      return;
+    }
+  
+    if(!empty($_POST["update"])){
+      self::update_access($group);
+    }
+  
+    echo "<h3>Change access for {$group["name"]}</h3>";
+    echo "<form method='post' action='#'>";
+    echo two_container("Show other tickets", "<input type='checkbox' name='showticket'".($group["showTicket"] == 1 ? " checked" : "").">");
+    echo two_container("Change group", "<input type='checkbox' name='changegroup'".($group["changeGroup"] == 1 ? " checked" : "").">");
+    echo two_container("Handle group", "<input type='checkbox' name='handleGroup'".($group["handleGroup"] == 1 ? " checked" : "").">");
+    echo two_container("Admin ticket", "<input type='checkbox' name='handleTickets'".($group["handleTickets"] == 1 ? " checked" : "").">");
+    echo two_container("Show error", "<input type='checkbox' name='showError'".($group["showError"] == 1 ? " checked" : "").">");
+    echo two_container("Show other profile", "<input type='checkbox' name='showProfile'".($group["showProfile"] == 1 ? " checked" : "").">");
+    echo "<div><input type='submit' name='update' value='Update access'></div>";
+    echo "</form>";
+  }
+  
+  private static function update_access(array $group){
+    $update = [];
+    if(!empty($_POST["showticket"]) && $group["showTicket"] == 0){
+      $update["showTicket"] = "1";
+    }elseif(empty($_POST["showticket"]) && $group["showTicket"] == 1){
+      $update["showTicket"] = "0";
+    }
+  
+    if(!empty($_POST["changegroup"]) && $group["changeGroup"] == 0){
+      $update["changeGroup"] = "1";
+    }elseif(empty($_POST["changegroup"]) && $group["changeGroup"] == 1){
+      $update["changeGroup"] = "0";
+    }
+  
+    if(!empty($_POST["handleGroup"]) && $group["handleGroup"] == 0){
+      $update["handleGroup"] = "1";
+    }elseif(empty($_POST["handleGroup"]) && $group["handleGroup"] == 1){
+      $update["handleGroup"] = "0";
+    }
+  
+    if(!empty($_POST["handleTickets"]) && $group["handleTickets"] == 0){
+      $update["handleTickets"] = "1";
+    }elseif(empty($_POST["handleTickets"]) && $group["handleTickets"] == 1){
+      $update["handleTickets"] = "0";
+    }
+  
+    if(!empty($_POST["showError"]) && $group["showError"] == 0){
+      $update["showError"] = "1";
+    }elseif(empty($_POST["showError"]) && $group["showError"] == 1){
+      $update["showError"] = "0";
+    }
+    
+    if(!empty($_POST["showProfile"]) && $group["showProfile"] == 0){
+      $update["showProfile"] = "1";
+    }elseif(empty($_POST["showProfile"]) && $group["showProfile"] == 1){
+      $update["showProfile"] = "0";
+    }
+  
+    if(count($update) > 0){
+      $sql = [];
+      foreach($update as $key => $value){
+        $sql[] = "`".$key."`='".intval($value)."'";
+      }
+      Database::get()->query("UPDATE `group` SET ".implode(",", $sql)." WHERE `id`='".$group["id"]."'");
+      html_okay("Access updated");
+    }else{
+      html_okay("No update detected");
+    }
+  
+    header("location: ?view=handleGroup&sub=Access&gid=".$group["id"]);
+    exit;
+  }
+}
